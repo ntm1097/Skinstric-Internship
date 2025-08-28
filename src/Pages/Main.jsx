3b3dd7ac4781689872a49cdef96a36551a2223e8
Nate <natemiller109725@gmail.com>
2025-08-27 23:13:19 -0400

    resolved incoming issues

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -5,11 +5,7 @@ import MainTitle from '../Components/MainTitle'
 const Main = () => {
   return (
     <div>
-<<<<<<< HEAD
       <Nav showButton={true} />
-=======
-      <Nav />
->>>>>>> 9ab84175fa962b5d24209adcd4fea94683e938ae
       <MainTitle />
     </div>
   )
